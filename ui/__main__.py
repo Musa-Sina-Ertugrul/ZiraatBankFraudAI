8973e4f9e7b1c5565edca6c3f5b28c7f6fc0db6f
Musa-Sina-Ertugrul <yutubems@gmail.com>
2024-08-31 15:49:39 +0300

    Updated for new model

--- ui/__main__.py
+++ ui/__main__.py
@@ -1,12 +1,17 @@
 from flask import Flask, request,render_template,flash
 import string
 import torch
+import pickle
 import torch.nn.functional as F
 from transformers import AutoModel,AutoModelForSequenceClassification,AutoModelForCausalLM,AutoTokenizer
 from models import ModelFraud,ModelSent
+import sklearn
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "./ui/uploaded"
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+with open('./ui/models/cc_fraud_model.pkl','rb') as f:
+    cc_fraud_model = pickle.load(f)
 
 tokenizer_sent = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
 tokenizer_sent.padding_side = "left"
@@ -23,6 +28,7 @@ model_sent = ModelSent().to("cuda")
 model_sent.load_state_dict(torch.load("./ui/models/model_sent.pth")["model_state_dict"])
 model_fraud.load_state_dict(torch.load("./ui/models/model_fraud.pth")["model_state_dict"])
 
+cc_fraud_result_table = {0:"Sahtecilik yok",1:"Sahtecilik olabilir"}
 fraud_result_table = {0:"Sahtecilik olabilir",1:"Sahtecilik yok"}
 sent_result_table = {0:"Negatif",1:"Notr",2:"Pozitif"}
 
@@ -53,6 +59,10 @@ def index():
 def fraud_analysis():
     return render_template("fraud_analysis.html")
 
+@app.route('/cc_fraud/')
+def cc_fraud_analysis():
+    return render_template("cc_fraud_analysis.html")
+
 @app.route('/upload_sentiment/', methods=['POST'])
 def upload_sentiment():
     text = request.form['main_text_input']
@@ -70,6 +80,20 @@ def upload_fraud():
     result = torch.argmax(model_fraud(preprocessed_text),dim=1).cpu().item()
     flash(fraud_result_table[result])
     return render_template("fraud_analysis.html")
+
+@app.route('/upload_cc_fraud/', methods=['POST'])
+def upload_cc_fraud():
+    step_count = float(request.form['main_input_step'])
+    trans_type = float(request.form['main_input_type'])
+    amount = float(request.form['main_input_amount'])
+    old_orig = float(request.form['main_input_old_orig'])
+    new_orig = float(request.form['main_input_new_orig'])
+    old_dest = float(request.form['main_input_old_dest'])
+    new_dest = float(request.form['main_input_new_dest'])
+    model_input = [[step_count,trans_type,amount,old_orig,new_orig,old_dest,new_dest]]
+    result = cc_fraud_model.predict(model_input)
+    flash(cc_fraud_result_table[int(result[0])])
+    return render_template("cc_fraud_analysis.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
